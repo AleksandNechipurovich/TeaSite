d837fe6471ff465a84a179012f8b405cf9bdb542
Aleksandr Nechipurovich <djsinnerakachap@rambler.ru>
2025-01-16 14:24:39 +0400

    создан проект

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('menu_icon').addEventListener('click', () => {
+    document.getElementById('header').classList.toggle('active');
+})
+
+document.getElementById('mobile_menu_close').addEventListener('click', () => {
+    document.getElementById('header').classList.toggle('active');
+})
